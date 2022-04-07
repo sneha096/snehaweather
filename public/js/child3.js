bc346dfc4c414854442a3818531285e08ca2b54c
S Snehalatha <55445142+sneha096@users.noreply.github.com>
2022-04-07 14:56:38 +0530

    Update child3.js

--- public/js/child3.js
+++ public/js/child3.js
@@ -1,4 +1,4 @@
-var tz = require('C:/Users/snehalatha/Desktop/child process/timeZone.js');
+var tz = require('https://github.com/sneha096/snehaweather/blob/main/timeZone.js');
 
 process.on('message', (body)=>{
     process.send(JSON.parse(JSON.stringify(tz.nextNhoursWeather(body.city_Date_Time_Name, body.hours, tz.allTimeZones()))));
