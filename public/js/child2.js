c16aa07f7687de2c7633305336d8186219d2d375
S Snehalatha <55445142+sneha096@users.noreply.github.com>
2022-04-07 14:56:23 +0530

    Update child2.js

--- public/js/child2.js
+++ public/js/child2.js
@@ -1,4 +1,4 @@
-var tz = require('C:/Users/snehalatha/Desktop/child process/timeZone.js');
+var tz = require('https://github.com/sneha096/snehaweather/blob/main/timeZone.js');
 
 process.on('message', (cityName)=>{
         process.send(JSON.parse(JSON.stringify(tz.timeForOneCity(cityName))));
