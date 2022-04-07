b8f383d5a25c79ee54f6941ce5817c6a6c7c82c1
S Snehalatha <55445142+sneha096@users.noreply.github.com>
2022-04-07 14:07:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname + "/public")));
 
 app.get('', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get("/all-timezone-cities", (req, res) => {
